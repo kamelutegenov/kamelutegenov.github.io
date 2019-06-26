3cc6b91dfaf71b9ff4083b9e79c15ded70ddab6b
kamelutegenov <kamel.utegenov@mail.ru>
2019-06-26 15:49:18 +0600

    Compass

---
+++ COMPASS/js/contacts.js
@@ -0,0 +1,16 @@
+$(function(){
+	$(".wrapper").click(function(){
+		$('.burger').toggleClass("db");
+	})
+
+	$(".icon_btn").click(function(){
+		$('#exampleModal').arcticmodal({
+			overlay: {
+		        css: {
+		            backgroundColor: '#004365',
+		            opacity: .7
+		        }
+		    }
+		});
+	});
+});
